c1861e87dcbd312e576d089d145d1b53132571e3
Sam Sharma <samsharma.dev@gmail.com>
2024-09-26 20:44:51 -0700

    create placeholder pages

---
+++ client/src/components/common/PageWrapper.jsx
@@ -0,0 +1,15 @@
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { setAppState } from "../../redux/features/appStateSlice";
+
+const PageWrapper = ({ state, children }) => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setAppState);
+  }, [state]);
+
+  return children;
+};
+
+export default PageWrapper;
