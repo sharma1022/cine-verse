f61c5d7ead4387562d3474197b643f226560c847
Sam Sharma <samsharma.dev@gmail.com>
2024-09-27 17:25:58 -0700

    add footer

--- client/src/components/layout/MainLayout.jsx
+++ client/src/components/layout/MainLayout.jsx
@@ -1,6 +1,8 @@
 import { Box } from "@mui/material";
 import { Outlet } from "react-router-dom";
 import GlobalLoading from "../common/GlobalLoading";
+import Footer from "../common/Footer";
+import Navbar from "../common/Navbar";
 
 const MainLayout = () => {
   return (
@@ -10,12 +12,14 @@ const MainLayout = () => {
       {/* Login Modal */}
       <Box display="flex" minHeight="100vh">
         {/* header */}
+        <Navbar />
         {/* main */}
         <Box component="main" flexGrow={1} overflow="hidden" minHeight="100vh">
           <Outlet />
         </Box>
       </Box>
       {/* footer */}
+      <Footer />
     </>
   );
 };
