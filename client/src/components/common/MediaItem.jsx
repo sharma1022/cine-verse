fd757c56ec61e307719b93579c062603055c173a
Sam Sharma <samsharma.dev@gmail.com>
2024-09-30 11:19:01 -0700

    update Midea Item

--- client/src/components/common/MediaItem.jsx
+++ client/src/components/common/MediaItem.jsx
@@ -1,4 +1,3 @@
-import PlayArrowIcon from "@mui/icons-material/PlayArrow";
 import { Box, Button, Stack, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
